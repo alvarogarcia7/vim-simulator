3d2b9932316e7ef2c10397737ceae8ad986c5469
Alvaro Garcia <derecerca@gmail.com>
2017-03-17 22:56:33 +0100

    Refactor: extract common, explaining function

--- test/vim_simulator/core_test.clj
+++ test/vim_simulator/core_test.clj
@@ -29,16 +29,17 @@
   [state command]
   (let [x (get-in state [:cursor :x])
         y (get-in state [:cursor :y])
-        line (get-in state [:buffer y])]
+        line (get-in state [:buffer y])
+        replace-in (fn [line-number new-line] (assoc-in state [:buffer line-number] new-line))]
     (case (:vim-simulator/command command)
       :vim-simulator/insert
       (let [[pre post] (map #(apply str %) (split-at x line))
             new-line (str pre (:vim-simulator/payload command) post)]
-        (assoc-in state [:buffer y] new-line))
+        (replace-in y new-line))
 
       :vim-simulator/append-at-end
       (let [new-line (str line (:vim-simulator/payload command))]
-        (assoc-in state [:buffer y] new-line)))))
+        (replace-in y new-line)))))
 
 
 (defn
